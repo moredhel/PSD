1f17c579a203d1e776fff808674a06b7e09c6244
hamhut1066 <hamhut1066@gmail.com>
2012-11-24 22:53:10 +0000

    added master page, with comments and basic outline

---
+++ share/master.php
@@ -0,0 +1,26 @@
+<?php #include a session_start method at the top to make use of global variables! ?>
+<html>
+<head>
+<!--Insert css linkage here -->
+<title><?php echo $title; ?></title><!--The title is loaded in Dynamically-->
+</head>
+<?php
+#This is an inclusion of the header webpage within the Master page
+require("header.php");
+#This is the Master Page,
+#all other pages are loaded through this page
+#This inserts the contents of the page into the master Page
+if($page_content == null)
+{
+	include("404.php");
+}
+else
+{
+	#insert try catch which redirects to 404 if caught
+	require($page_content);
+}
+#insert footer for the master page here!
+require("footer.php");
+?>
+<!-- Any Scripts could either be included in a seperate file in the head or put in the bottom here-->
+</html>
