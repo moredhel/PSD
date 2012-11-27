4f6919ca167f369b3fa1e01c817352af295254fa
hamhut1066 <hamhut1066@gmail.com>
2012-11-27 16:47:39 +0000

    added serverside user input validation, strlength > 0

--- lib/login.php
+++ lib/login.php
@@ -3,11 +3,12 @@
 	$suffix = "?";
 	//remember to add anti sql-injection code!!!	
 	//check if form has been submitted or someone has direct url'd here
-	if(isset($_POST['click']) && $_POST['username'].length > 0)
+	if(isset($_POST['click']) && strlen($_POST['username']) > 0 && strlen($_POST['password']) > 0)
 	{
 		$_SESSION['username'] = $_POST['username'];
         #header( 'Location: ../share/home.php' ) ;		
 		//do database and user auth stuff here
+		echo strlen($_SESSION['username']);
 	}
 	else{$suffix = "?error=0";}
 		header( "Location: ../share/home.php$suffix" ) ;
