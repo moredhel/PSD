52fa9fda19896fdd338bcc5b7875cb9ef096a64c
hamhut1066 <hamhut1066@gmail.com>
2012-11-27 14:52:25 +0000

    getting basic logic into login process

--- lib/login.php
+++ lib/login.php
@@ -1,3 +1,12 @@
 <?php
 	//remember to add anti sql-injection code!!!	
+	//check if form has been submitted or someone has direct url'd here
+	if(isset($_POST['click']))
+	{
+		echo "Welcome " , $_POST['username'];
+	}
+	else
+	{
+		echo 'Go Away';
+	}
 ?>
