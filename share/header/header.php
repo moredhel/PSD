a91ef4afecf4ba8e152df2282045b48992eb69a0
hamhut1066 <hamhut1066@gmail.com>
2012-11-27 13:43:27 +0000

    edited login form

--- share/header/header.php
+++ share/header/header.php
@@ -17,12 +17,7 @@ if(isset($_SESSION['userid']))
 else
 {
 	//display login form
-	echo "	<form action='../lib/login.php' method='POST'>
-	<p>Username:<input type='text' name='username' /></p>
-	<p>Password:<input type='password' name='password' /></p>
-					<input type='hidden' name='click' />
-					<input type='submit' value='login' />
-			</form> ";
+	include('header/login_form.html');
 }
 ?>
 </div>
--- share/header/login_form.php
+++
@@ -1,6 +0,0 @@
-<form action='../lib/login.php' method='POST'>
-    <p>Username:<input type='text' name='username' /></p>
-    <p>Password:<input type='password' name='password' /></p>
-                    <input type='hidden' name='click' />
-                    <input type='submit' value='login' />
-</form>
