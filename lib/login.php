7ef813b1144ed287b4f1409ac3a438782a0d568a
hamhut1066 <hamhut1066@gmail.com>
2012-11-30 21:48:08 +0000

    added password encryption using md5, login/out works, now for register

--- lib/login.php
+++ lib/login.php
@@ -2,24 +2,24 @@
 	session_start();
 	#include database class
 	require("../classes/db_connect.php");#connection to database
-	require("../classes/salt.php");
+	require("../lib/salt.php");
 	$suffix = "?";
 	//remember to add anti sql-injection code!!!	
 	//check if form has been submitted or someone has direct url'd here
-	$db = new db();
-	$dataset = $db->Request("SELECT username, password FROM psd_users WHERE username LIKE 'Hamish'");
-	for($i = 0; $i < count($dataset); $i++)
-	{
-		$row = $dataset[$i];
-		echo "</br>";
-		echo $row['username'] , ":" , $row['password'];
-	} 
 	if(isset($_POST['click']) && strlen($_POST['username']) > 0 && strlen($_POST['password']) > 0)
 	{
-		$_SESSION['username'] = $_POST['username'];
+		$db = new db();
+		$dataset = $db->Request("SELECT username, password FROM psd_users WHERE username LIKE '" . $_POST['username'] . "'");
+		$row = $dataset[0];
+		if($row['password'] == encrypt($_POST['password']))
+		{
+			$_SESSION['username'] = $_POST['username'];
+			echo "Success";
+		}
+		else{$suffix = "?error=0";}
         #header( 'Location: ../share/home.php' ) ;		
 		//do database and user auth stuff here
 	}
 	else{$suffix = "?error=0";}
-	#	header( "Location: ../share/home.php$suffix" ) ;
+	header( "Location: ../share/home.php$suffix" ) ;
 ?>
