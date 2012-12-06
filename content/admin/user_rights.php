3e5240cbe1ba6adaffcd083e0e211aff77f7b6e4
hamhut1066 <hamhut1066@gmail.com>
2012-12-06 23:26:06 +0000

    added files to content/admin

---
+++ content/admin/user_rights.php
@@ -0,0 +1,5 @@
+<?php
+	#this file will display all the users in a list, allow you to select a user and update their credentials!
+	#there will be a search function to filter resultss, the first loading of the page will limit the count to <30>
+	:
+?>
