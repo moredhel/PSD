7ae06504d626464c00455f5372437ffe101fbadc
hamhut1066 <hamhut1066@gmail.com>
2012-12-07 01:35:25 +0000

    fixed change password

--- lib/newpassword.php
+++ lib/newpassword.php
@@ -12,13 +12,14 @@
 	$db = new db();
 	$dataset = $db->Request("SELECT password FROM psd_users WHERE userid = ". $_SESSION['userid']); 
 	$row = $dataset[0];
-	echo "1";
-	if(encrypt($_POST['oldpassword']) == $row['password'] && strlen($_POST['password'] > 0 ))
+	if(encrypt($_POST['oldpassword']) == $row['password'] && strlen($_POST['password']) > 0 )
 	{
 		#code to check that the new passwords match
 		if($_POST['password'] == $_POST['confirm'])
 		{
-			echo "game";
+			$sql = "UPDATE psd_users SET password = '" . encrypt($_POST['password']) . "' WHERE userid = " . $_SESSION['userid'];
+			$db->Ins($sql);
 		}
 	}
+	header("Location: ../share/preferences.php");
 ?>
