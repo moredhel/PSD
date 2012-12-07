0a234d456fabfc60107233df384c496bf442d0dc
hamhut1066 <hamhut1066@gmail.com>
2012-12-07 01:12:59 +0000

    added some data validation to newpassword.php

--- lib/newpassword.php
+++ lib/newpassword.php
@@ -7,5 +7,18 @@
 	}
 	#include databse class, encrypt class
 	require("../classes/db_connect.php");
-	require("../classes/salt.php");
+	require("../lib/salt.php");
+	#first check if the password is correct for the username
+	$db = new db();
+	$dataset = $db->Request("SELECT password FROM psd_users WHERE userid = ". $_SESSION['userid']); 
+	$row = $dataset[0];
+	echo "1";
+	if(encrypt($_POST['oldpassword']) == $row['password'] && strlen($_POST['password'] > 0 ))
+	{
+		#code to check that the new passwords match
+		if($_POST['password'] == $_POST['confirm'])
+		{
+			echo "game";
+		}
+	}
 ?>
