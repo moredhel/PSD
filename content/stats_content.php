6b104567fcbb11968d3d87e3c5aae7ff5eb026d2
hamhut1066 <hamhut1066@gmail.com>
2012-12-11 12:36:59 +0300

    added products table and companies can own products

--- content/stats_content.php
+++ content/stats_content.php
@@ -1,5 +1,23 @@
+<table id='stats_products'>
 <?php
 	#This is where all the stats for the companies are manipulated
 	require("../classes/db_connect.php");
 	$db = new db();
+	$sql = "SELECT c.CompanyName, p.productname, q.quantity FROM psd_companies c, psd_products p, psd_quantities q WHERE c.companyId = q.companyId AND p.productId = q.productId";
+	$dataset = $db->Request($sql);
+	
+	for($i = 0; $i < count($dataset); $i++)
+	{
+		$row = $dataset[$i];
+		echo "<tr><td>";
+		echo $row['CompanyName'];
+		echo "</td><td>";
+		echo $row['productname'];
+		echo "</td><td>";
+		echo $row['quantity'];
+		echo "</td></tr>";
+		
+	}	
+
 ?>
+</table>
